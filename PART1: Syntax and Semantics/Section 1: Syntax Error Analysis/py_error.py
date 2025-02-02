63faef35fb067b027ae8f0ec00df445ae446ac8c
PabitraBhandari <89492267+PabitraBhandari@users.noreply.github.com>
2025-02-02 12:01:24 -0600

    Create py_error.py

---
+++ PART1: Syntax and Semantics/Section 1: Syntax Error Analysis/py_error.py
@@ -0,0 +1,9 @@
+def calculate_sum(arr  # Missing closing parenthesis in function definition
+    total = o  # 'o' is not defined; should be '0'
+    for num in arr  # Missing colon at the end of the for loop
+        total += num
+    return total
+
+numbers = [1, 2, 3, 4, 5]
+result = calculate_sum (numbers)
+print("Sum in Pyhton :", result)  # Typographical error in string
